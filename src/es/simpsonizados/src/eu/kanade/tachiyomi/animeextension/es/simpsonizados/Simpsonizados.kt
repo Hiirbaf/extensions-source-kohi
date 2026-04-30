7e9942aff13da0025acd2a153e20abfbc8dd0a1e
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-04-29 21:06:26 -0300

    Add season episode parsing to Simpsonizados extension

--- src/es/simpsonizados/src/eu/kanade/tachiyomi/animeextension/es/simpsonizados/Simpsonizados.kt
+++ src/es/simpsonizados/src/eu/kanade/tachiyomi/animeextension/es/simpsonizados/Simpsonizados.kt
@@ -1,8 +1,12 @@
 package eu.kanade.tachiyomi.animeextension.es.simpsonizados
 
+import eu.kanade.tachiyomi.animesource.model.SEpisode
 import eu.kanade.tachiyomi.multisrc.dooplay.DooPlay
 import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Request
+import okhttp3.Response
+import org.jsoup.nodes.Element
 
 class Simpsonizados : DooPlay(
     lang = "es",
@@ -21,4 +25,46 @@ class Simpsonizados : DooPlay(
 
     override fun popularAnimeNextPageSelector() =
         "div.resppages > a > span.fa-chevron-right"
+
+    // La estructura del sitio es:
+    // div con número de season > ul > li > a (episodio)
+    override val seasonListSelector = "div#seasons > div"
+
+    override fun getSeasonEpisodes(season: Element): List<SEpisode> {
+        val seasonNum = season.selectFirst("div[id^=season-]")
+            ?.id()
+            ?.removePrefix("season-")
+            ?: season.selectFirst("span, div")?.text()?.trim()
+            ?: "?"
+
+        return season.select("ul > li").mapNotNull { li ->
+            runCatching {
+                val a = li.selectFirst("a[href]") ?: return@runCatching null
+                val epNum = a.text().substringBefore(" - ").trim()
+                SEpisode.create().apply {
+                    name = "$episodeSeasonPrefix $seasonNum x $epNum - ${a.text().substringAfter(" - ").trim()}"
+                    episode_number = epNum.toFloatOrNull() ?: 0F
+                    date_upload = li.selectFirst("span.date, span")
+                        ?.text()?.toDate() ?: 0L
+                    setUrlWithoutDomain(a.attr("href"))
+                }
+            }.getOrNull()
+        }
+    }
+
+    override fun episodeListParse(response: Response): List<SEpisode> {
+        val doc = getRealAnimeDoc(response.asJsoup())
+        val seasons = doc.select(seasonListSelector)
+        return if (seasons.isEmpty()) {
+            listOf(
+                SEpisode.create().apply {
+                    setUrlWithoutDomain(doc.location())
+                    episode_number = 1F
+                    name = episodeMovieText
+                },
+            )
+        } else {
+            seasons.flatMap(::getSeasonEpisodes).reversed()
+        }
+    }
 }
